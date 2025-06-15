d0f3a71c27f8290560d56b80b38d408e0366a29f
medAndro <med4ndro@gmail.com>
2025-06-16 03:32:20 +0900

    feat: ViewModel에 NaverMap 객체 연동

--- app/src/main/java/com/medandro/bikewidget/ui/map/MapFragment.kt
+++ app/src/main/java/com/medandro/bikewidget/ui/map/MapFragment.kt
@@ -7,10 +7,17 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.medandro.bikewidget.databinding.FragmentMapBinding
+import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 
-class MapFragment : Fragment() {
+class MapFragment :
+    Fragment(),
+    OnMapReadyCallback {
+    private lateinit var mapView: MapView
+    private lateinit var mapViewModel: MapViewModel
+
     private var _binding: FragmentMapBinding? = null
-
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -18,8 +25,7 @@ class MapFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        val mapViewModel =
-            ViewModelProvider(this).get(MapViewModel::class.java)
+        mapViewModel = ViewModelProvider(this).get(MapViewModel::class.java)
 
         _binding = FragmentMapBinding.inflate(inflater, container, false)
         val root: View = binding.root
@@ -27,8 +33,53 @@ class MapFragment : Fragment() {
         return root
     }
 
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView = binding.mapView
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        mapViewModel.onMapReady(naverMap)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
+        mapView.onDestroy()
         _binding = null
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 }
--- app/src/main/java/com/medandro/bikewidget/ui/map/MapViewModel.kt
+++ app/src/main/java/com/medandro/bikewidget/ui/map/MapViewModel.kt
@@ -1,5 +1,26 @@
 package com.medandro.bikewidget.ui.map
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.overlay.Marker
 
-class MapViewModel : ViewModel()
+class MapViewModel : ViewModel() {
+    private val _naverMap = MutableLiveData<NaverMap>()
+    val naverMap: LiveData<NaverMap> = _naverMap
+
+    fun onMapReady(naverMap: NaverMap) {
+        _naverMap.value = naverMap
+        addMarkerAtCurrentLocation()
+    }
+
+    fun addMarkerAtCurrentLocation() {
+        val currentMap = naverMap.value ?: return
+
+        val marker = Marker()
+        marker.position = LatLng(37.5670135, 126.9783740)
+        marker.map = currentMap
+    }
+}
